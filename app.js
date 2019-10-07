6afa7c3d45de64e82392c4451884034e8400cea2
NeerajKumar22 <ineerajkr@gmail.com>
2019-10-07 23:36:59 +0530

    add init function and call setupEventListner in the function

--- app.js
+++ app.js
@@ -36,14 +36,25 @@ var UIController = (function () {
 // GLOBAL App controller
 var controller = (function (budgetCtrl, UICtrl) {
 
-	var DOM = UICtrl.getDOMstrings();
+	var setupEventListeners = function () {
+
+		var DOM = UICtrl.getDOMstrings();
+
+		document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+
+		document.addEventListener("keypress", function (event) {
+			if (event.keyCode === 13 || event.which === 13) {
+				ctrlAddItem();
+			}
+		})
+
+	};
 
 	var ctrlAddItem = function () {
 
 		// 1. Get the field input data
 
 		var input = UICtrl.getInput();
-		console.log(input);
 
 		// 2. Add the item to the budget controller.
 
@@ -55,12 +66,14 @@ var controller = (function (budgetCtrl, UICtrl) {
 
 	}
 
-	document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
-
-	document.addEventListener("keypress", function (event) {
-		if (event.keyCode === 13 || event.which === 13) {
-			ctrlAddItem();
+	return {
+		init: function () {
+			console.log("Application has started.")
+			setupEventListeners();
 		}
-	})
+	}
 
 })(budgetController, UIController);
+
+
+controller.init();
